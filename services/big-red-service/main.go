237ae3473c897019deb7f2e88249b35f62ff2060
VseZnal <Vseznal66@gmail.com>
2023-03-19 01:26:14 +0300

    chore: go mod + main

--- services/big-red-service/main.go
+++ services/big-red-service/main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"big-red/services/big-red-service/proto"
+	big_red_service "big-red/services/big-red-service/proto/big-red-service"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", "8090")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+
+	big_red_service.RegisterBigRedServiceServer(grpcServer, proto.Server{})
+	grpcServer.Serve(lis)
+}
